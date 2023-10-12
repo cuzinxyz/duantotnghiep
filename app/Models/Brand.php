eb4b10279b3048e17ab56ba7af1b1ee5c51d77c1
manhtuonglam9703 <tuonglm9703@gmail.com>
2023-10-12 22:53:21 +0700

    brands

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Brand extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $guarded = ['id'];
+
+    public function user(): BelongsTo {
+      return $this->belongsTo(User::class);
+    }
 }
