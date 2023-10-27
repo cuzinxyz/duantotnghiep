59b4b9a256e8b213df3ef8b75a09b96bb5d0843a
Cuzin Pro <67429817+cuzinxyz@users.noreply.github.com>
2023-10-27 22:20:51 +0700

    update 1 so thu

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,6 @@ use App\Http\Controllers\ServiceController;
 use App\Models\Service;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,28 +22,35 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
+Route::controller(HomeController::class)->group(function () {
+  Route::get('/', 'index')->name('homepage');
+});
 
-Route::get("/service/{idService}", function($idService) {
-  $serv = Service::findOrFail($idService);
+Route::controller(CarController::class)->group(function () {
+  Route::get('/dang-tin-ban-xe', 'sellCar');
+});
 
-  return view('service-detail', compact('serv'));
-})->name('service.detail');
+Route::controller(ServiceController::class)->group(function () {
+  Route::get('/danh-sach-dich-vu', 'index');
+  Route::get('/dich-vu/{idService}', 'detail')->name('service.detail');
+});
 
-Route::post('payment', [CheckOutController::class, 'checkout'])->name('payment-vnpay');
+Route::controller(CheckOutController::class)->group(function () {
+   # payment
+   Route::post('/payment', 'checkout')->name('payment-vnpay');
+   # result after payment
+   Route::get('/ket-qua', 'result')->name('resultAfterPayment');
+});
 
-Route::get('handle-payment', [CheckOutController::class, 'handlePayment'])->name('handlePayment');
 
-Route::get('/info', [HomeController::class, 'info']);
+// Route::post('payment', [CheckOutController::class, 'checkout'])->name('payment-vnpay');
 
-Route::get('/info', [HomeController::class, 'info']);
-Route::get('/service', [ServiceController::class, 'index'])->name('service');
+// Route::get('handle-payment', [CheckOutController::class, 'handlePayment'])->name('handlePayment');
+
 
 // Route::match(['GET','POST'],'/dang-tin-ban-xe',[CarController::class,'sellCar'])->name('sellCar');
 
 Route::get('/dang-xe', [CarController::class, 'sellCar']);
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
@@ -54,4 +60,4 @@ Route::get('manage-post', function () {
 Route::get('push-news', function () {
     return view('push-news');
 });
-  
+
