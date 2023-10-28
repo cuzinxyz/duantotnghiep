5e7841e71aa25dd102e63995ec0c52099839c1e5
nguyentuan <nguyenvantuan18112003dt@gmail.com>
2023-10-28 20:06:56 +0700

    fix error slug car

--- database/migrations/2023_10_06_134701_create_cars_table.php
+++ database/migrations/2023_10_06_134701_create_cars_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
     Schema::create('cars', function (Blueprint $table) {
       $table->id();
       $table->string('title');
-      $table->string('slug');
       $table->integer('price');
       $table->integer('user_id');
       $table->integer('brand_id');
