2cb38677f49dce749412e46b2afabbd1101dd006
Cuzin Pro <67429817+cuzinxyz@users.noreply.github.com>
2023-11-24 22:02:13 +0700

    Update form-dang-tin.blade.php

--- resources/views/livewire/form-dang-tin.blade.php
+++ resources/views/livewire/form-dang-tin.blade.php
@@ -42,167 +42,140 @@
                                                 wire:model="model_select">
                                                 <option value="0">-- Chọn model --</option>
 
-                                    @foreach ($models as $model)
-                                        <option value="{{ $model->id }}">{{ $model->model_name }}</option>
-                                    @endforeach
-                                </select>
-                                <div>
-                                    @error('model_select')
-                                        {{ $message }}
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
+                                                @foreach ($models as $model)
+                                                    <option value="{{ $model->id }}">{{ $model->model_name }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                            <div class="text-danger">
+                                                @error('model_select')
+                                                    {{ $message }}
+                                                @enderror
+                                            </div>
+                                        </div>
+                                    </div>
 
-                        {{-- Tên xe thuộc hãng --}}
+                                    {{-- Tên xe thuộc hãng --}}
 
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Nhiên liệu</label>
+                                            <select class="nice-select beauty-shadow form-control-lg" wire:model="fuel">
+                                                <option value="0">-- Chọn loại nhiên liệu --</option>
+                                                @foreach ($fuels as $fuel)
+                                                    <option>{{ $fuel }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('fuel')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Hộp số</label>
-                                <select class="nice-select" wire:model="transmission">
-                                    <option value="0">-- Chọn hộp số --</option>
-                                    <option value="sotay">Số tay</option>
-                                    <option value="sotudong">Số tự động</option>
-                                </select>
-                            </div>
-                            <div>
-                                @error('transmission')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Số chỗ ngồi</label>
+                                            <select class="nice-select beauty-shadow form-control-lg"
+                                                wire:model="number_of_seats">
+                                                <option value="">--Chọn số chỗ ngồi--</option>
+                                                @foreach ($seats as $seat)
+                                                    <option value="{{ $seat }}">{{ $seat }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('number_of_seats')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Nhiên liệu</label>
-                                <select class="nice-select" wire:model="fuel">
-                                    <option value="0">-- Chọn loại nhiên liệu --</option>
-                                    @foreach ($fuels as $fuel)
-                                        <option>{{ $fuel }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div>
-                                @error('fuel')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Phiên bản</label>
+                                            <input type="text" wire:model="version"
+                                                placeholder="Hãy nhập phiên bản xe (nếu có)">
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('version')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Số chỗ ngồi</label>
-                                <input wire:model="number_of_seats" type="number" placeholder="Nhập số chỗ ngồi">
-                            </div>
-                            <div>
-                                @error('number_of_seats')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Năm sản xuất</label>
+                                            <select class="nice-select beauty-shadow form-control-lg"
+                                                wire:model="year_of_manufacture">
+                                                <option value="0">-- Chọn năm sản xuất
+                                                    <link rel="stylesheet"
+                                                        href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
+                                                        integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ"
+                                                        crossorigin="anonymous">
+                                                </option>
+                                                @foreach ($years as $year)
+                                                    <option>{{ $year }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('year_of_manufacture')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Màu sắc</label>
-                                <select class="nice-select" wire:model="color">
-                                    <option value="0">-- Chọn màu xe --</option>
-                                    @foreach ($colors as $color)
-                                        <option>{{ $color }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div>
-                                @error('color')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Số KM đã đi*</label>
+                                            <input wire:model="mileage" type="number" placeholder="Nhập số km đã đi">
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('mileage')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Phiên bản</label>
-                                <input name="version" type="text" wire:model="version" placeholder="Hãy nhập phiên bản xe (nếu có)">
-                            </div>
-                            <div>
-                                @error('version')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Giá muốn bán*</label>
+                                            <input wire:model="price" type="text" placeholder="VND">
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('price')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Năm sản xuất</label>
-                                <select name="year_of_manufacture" class="nice-select" wire:model="year_of_manufacture">
-                                    <option value="0">-- Chọn năm sản xuất
-                                        <link rel="stylesheet"
-                                            href="https://use.fontawesome.com/releases/v5.1.1/css/all.css"
-                                            integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ"
-                                            crossorigin="anonymous">
-                                    </option>
-                                    @foreach ($years as $year)
-                                        <option>{{ $year }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div>
-                                @error('year_of_manufacture')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Tình trạng</label>
-                                <select class="nice-select" wire:model="condition">
-                                    <option value="0">-- Chọn tình trạng --</option>
-                                    <option value="xedaquasudung">Xe đã qua sử dụng</option>
-                                    <option value="xemoi">Xe mới</option>
-                                </select>
-                            </div>
-                            <div>
-                                @error('condition')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-6 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Mã lực*</label>
+                                            <input wire:model="engine" type="text" placeholder="Nhập số mã lực">
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('title')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Số KM đã đi*</label>
-                                <input wire:model="mileage_traveled" type="text" placeholder="Nhập số km đã đi">
-                            </div>
-                            <div>
-                                @error('mileage_traveled')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Giá muốn bán*</label>
-                                <input wire:model="price" type="text" placeholder="VND">
-                            </div>
-                            <div>
-                                @error('price')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="col-md-6 mb-20">
-                            <div class="form-inner">
-                                <label>Tiêu đề*</label>
-                                <input wire:model="title" type="text" placeholder="Nhập tiêu đề cho bài đăng">
-                            </div>
-                            <div>
-                                @error('title')
-                                    {{ $message }}
-                                @enderror
-                            </div>
-                        </div>
+                                    <div class="col-md-12 mb-20">
+                                        <div class="form-inner">
+                                            <label class="fontAvantRegular">Tiêu đề*</label>
+                                            <input wire:model="title" type="text"
+                                                placeholder="Nhập tiêu đề cho bài đăng">
+                                        </div>
+                                        <div class="text-danger">
+                                            @error('title')
+                                                {{ $message }}
+                                            @enderror
+                                        </div>
+                                    </div>
 
                                     <div class="col-md-12">
                                         <div class="form-inner">
