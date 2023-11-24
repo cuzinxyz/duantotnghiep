efc40814097a4aaee0546c3c54f6b3ca4f399c6a
HaiDuong003 <duongph24202@gmail.com>
2023-11-24 21:51:11 +0700

    report

--- resources/views/components/detailpage/info-car.blade.php
+++ resources/views/components/detailpage/info-car.blade.php
@@ -337,29 +337,27 @@
         </div>
         {{-- share , report btn  --}}
         <div class="single-item mb-50" id="btn-lists">
-            <div class="more-options d-flex flex-row-reverse align-items-center gap-3">
-                <button class="bookmarkBtn">
+            <div class="more-options d-flex flex-row-reverse align-items-center gap-3" x-data="{ open: false }">
+                <button class="bookmarkBtn" @click="open = !open">
                     <span class="IconContainer">
-                        <svg fill="white" viewBox="0 0 512 512" height="1em">
-                            <path
-                                d="M123.6 391.3c12.9-9.4 29.6-11.8 44.6-6.4c26.5 9.6 56.2 15.1 87.8 15.1c124.7 0 208-80.5 208-160s-83.3-160-208-160S48 160.5 48 240c0 32 12.4 62.8 35.7 89.2c8.6 9.7 12.8 22.5 11.8 35.5c-1.4 18.1-5.7 34.7-11.3 49.4c17-7.9 31.1-16.7 39.4-22.7zM21.2 431.9c1.8-2.7 3.5-5.4 5.1-8.1c10-16.6 19.5-38.4 21.4-62.9C17.7 326.8 0 285.1 0 240C0 125.1 114.6 32 256 32s256 93.1 256 208s-114.6 208-256 208c-37.1 0-72.3-6.4-104.1-17.9c-11.9 8.7-31.3 20.6-54.3 30.6c-15.1 6.6-32.3 12.6-50.1 16.1c-.8 .2-1.6 .3-2.4 .5c-4.4 .8-8.7 1.5-13.2 1.9c-.2 0-.5 .1-.7 .1c-5.1 .5-10.2 .8-15.3 .8c-6.5 0-12.3-3.9-14.8-9.9c-2.5-6-1.1-12.8 3.4-17.4c4.1-4.2 7.8-8.7 11.3-13.5c1.7-2.3 3.3-4.6 4.8-6.9c.1-.2 .2-.3 .3-.5z">
-                            </path>
-                        </svg>
+                        <i class="text-white bi bi-flag-fill"></i>
                     </span>
-                    {{-- <button type="button" class="text mb-0" data-bs-toggle="modal" data-bs-target="#report">
+                    <p class="text mb-0 dialog__trigger">
                         Báo Cáo
-                    </button> --}}
-                    <button class="text mb-0 dialog__trigger">Báo
-                        Cáo</button>
-                    <div class="dialog">
-                        <span class="dialog__close">✕</span>
-                        <h2 class="dialog__title">This is a title</h2>
-                        <p class="dialog__content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit
-                            necessitatibus illo deleniti doloremque culpa voluptas recusandae, sunt eligendi amet, ut
-                            modi voluptatibus ipsa quas voluptatem consectetur atque, velit reprehenderit debitis.</p>
-                        &lt;button class="dialog__action"&gt;Read more →&lt;/button&gt;
-                    </div>
+                    </p>
                 </button>
+
+                <div class="report-box" x-show="open">
+                    <div class="dialog" @click.outside="open = false">
+                        <span class="dialog__close" @click="open = false">&#x2715;</span>
+                        <h2 class="dialog__title">This is a title</h2>
+                        <ul class="dialog__content">
+                            <li></li>
+                        </ul>
+                        <button class="dialog__action">Read more &#8594;</button>
+                    </div>
+                </div>
+
                 <livewire:add-to-wish-list carID="{{ $carDetail->id }}" />
             </div>
         </div>
@@ -534,51 +532,5 @@
                 fill: coral;
             }
         </style>
-    @endpush
-    @push('scripts')
-        <script>
-            function dialog() {
-
-                // Declare variables
-                var dialogBox = $('.dialog'),
-                    dialogTrigger = $('.dialog__trigger'),
-                    dialogClose = $('.dialog__close'),
-                    dialogTitle = $('.dialog__title'),
-                    dialogContent = $('.dialog__content'),
-                    dialogAction = $('.dialog__action');
-
-                // Open the dialog
-                dialogTrigger.on('click', function(e) {
-                    dialogBox.toggleClass('dialog--active');
-                    e.stopPropagation();
-                });
-
-                // Close the dialog - click close button
-                dialogClose.on('click', function() {
-                    dialogBox.removeClass('dialog--active');
-                });
-
-                // Close the dialog - press escape key // key#27
-                $(document).keyup(function(e) {
-                    if (e.keyCode === 27) {
-                        dialogBox.removeClass('dialog--active');
-                    }
-                });
-
-                // Close the dialog - click outside
-                $(document).on('click', function(e) {
-                    if ($(e.target).is(dialogBox) === false &&
-                        $(e.target).is(dialogTitle) === false &&
-                        $(e.target).is(dialogContent) === false &&
-                        $(e.target).is(dialogAction) === false) {
-                        dialogBox.removeClass('dialog--active');
-                    }
-                });
-
-            }
-
-            // Run function
-            $(dialog);
-        </script>
     @endpush
 </div>
