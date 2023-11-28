4746c5009f6d9825c5e0ebe3b660270784235d68
HaiDuong003 <duongph24202@gmail.com>
2023-11-28 20:20:55 +0700

    report

--- app/Http/Controllers/CarDetailController.php
+++ app/Http/Controllers/CarDetailController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class CarDetailController extends Controller
 {
-    public function index($slug = null)
+    public function index(Request $request, $slug = null)
     {
         $carDetail = [];
         if ($slug) {
@@ -17,7 +17,9 @@ class CarDetailController extends Controller
         views($carDetail)->record();
 
         $recentCars = Car::inRandomOrder()->limit(4)->get();
-
+        if ($request->isMethod('POST')) {
+            dd($request);
+        }
         return view('detail', compact('carDetail', 'recentCars'));
     }
 }
--- resources/views/components/detailpage/info-car.blade.php
+++ resources/views/components/detailpage/info-car.blade.php
@@ -347,37 +347,7 @@
                     </p>
                 </button>
 
-                <div class="report-box" x-show="open">
-                    <div class="dialog" @click.outside="open = false">
-                        <span class="dialog__close" @click="open = false">&#x2715;</span>
-                        <h2 class="dialog__title">Hãy chọn vấn đề</h2>
-                        <div class="dialog__content">
-                            <div class="list-group mt-3 list-group-light">
-                                <div class="item_report mb-2">
-                                    <input class="form-check-input" type="checkbox" value="Lừa đảo"
-                                        id="luadao" />
-                                    <label for="luadao">Lừa đảo</label>
-                                </div>
-                                <div class="item_report mb-2">
-                                    <input class="form-check-input" type="checkbox"
-                                        value="Hình ảnh không đạt tiêu chuẩn cộng đồng" id="khoathan" />
-                                    <label for="khoathan">Hình ảnh không đạt tiêu chuẩn cộng đồng</label>
-                                </div>
-                                <div class="item_report mb-2">
-                                    <input class="form-check-input" type="checkbox" value="Thông tin sai sự thật"
-                                        id="saithongtin" />
-                                    <label for="saithongtin">Thông tin sai sự thật</label>
-                                </div>
-                                <div class="item_report mb-2">
-                                    <input class="form-check-input" type="checkbox" value="Ngôn ngữ gây thù ghét"
-                                        id="ngonngu" />
-                                    <label for="ngonngu">Ngôn ngữ gây thù ghét</label>
-                                </div>
-                            </div>
-                        </div>
-                        <button class="dialog__action">Báo cáo&#8594;</button>
-                    </div>
-                </div>
+                <livewire:report />
 
                 <livewire:add-to-wish-list carID="{{ $carDetail->id }}" />
             </div>
