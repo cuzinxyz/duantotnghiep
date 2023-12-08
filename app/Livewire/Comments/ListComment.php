51a9c384bcae8d19ac8dd0c46102d92ffebc097e
Cuzin Pro <67429817+cuzinxyz@users.noreply.github.com>
2023-12-08 23:35:30 +0700

    update comment

--- app/Livewire/Comments/ListComment.php
+++ app/Livewire/Comments/ListComment.php
@@ -17,115 +17,77 @@ class ListComment extends Component
 {
     use WithPagination;
 
-    public $comment;
-
-    public $slug;
-
-    public $page;
-
+    #[Locked]
+    public $oop;
     public $reply;
 
-    public function mount($id = null)
+    public function mount($oop)
     {
-
-        $url_page = request()->url();
-
-        $this->slug = last(explode('/', $url_page));
-
-        $this->page = Route::currentRouteName();
+        $this->oop = $oop;
     }
 
-    public function replyComment($id = null)
+    #[On('comment-created')]
+    public function renderComment()
+    {
+        if ($this->oop instanceof News) {
+            return CommentsModel::with([
+                'user', 'reply' => function ($query) {
+                    $query->orderBy('created_at', 'desc');
+                }
+            ])
+                ->where('news_id', $this->oop->id)
+                ->orderBy('created_at', 'desc')
+                ->simplePaginate(4);
+        } elseif ($this->oop instanceof Car) {
+            return CommentsModel::with([
+                'user', 'reply' => function ($query) {
+                    $query->orderBy('created_at', 'desc');
+                }
+            ])
+                ->where('car_id', $this->oop->id)
+                ->orderBy('created_at', 'desc')
+                ->simplePaginate(4);
+        }
+    }
+
+    public function replyComment($item)
     {
         if (!Auth::check()) {
             $this->dispatch('showError', 'Vui lòng đăng nhập để bình luận');
             return redirect()->route('login');
         }
-
-
-        $user_id = Auth::user()->id;
-        $comment_id = $id;
-
-        if ($this->page == 'car-detail') {
-            $carComment = Car::where('slug', $this->slug)->first();
-
+        $this->validate(['reply' => 'required|min:3']);
+        if ($item['car_id'] != 0) {
+            // dd($item);
             ReplyComments::create([
                 'body' => htmlspecialchars($this->reply),
-                'comment_id' => $comment_id,
-                'user_id' => $user_id,
-                'car_id' => $carComment->id,
+                'comment_id' => $item['id'],
+                'user_id' => auth()->id(),
+                'car_id' => $item['car_id'],
                 'news_id' => 0
             ]);
-
+            // dd('hihi');
             $this->reset('reply');
-
+            $item = null;
+            $this->dispatch('renderReplyComments');
+        } elseif ($item['news_id'] != 0) {
+            ReplyComments::create([
+                'body' => htmlspecialchars($this->reply),
+                'comment_id' => $item['id'],
+                'user_id' => auth()->id(),
+                'car_id' => 0,
+                'news_id' => $item['news_id']
+            ]);
+            $this->reset('reply');
+            $item = null;
             $this->dispatch('renderReplyComments');
         }
-
-        if ($this->page == 'news.index') {
-            $slug = str_replace('.html', '', $this->slug);
-            $newComment = News::where('slug', $slug)->first();
-
-            if (strlen($this->comment) > 0) {
-                ReplyComments::create([
-                    'body' => htmlspecialchars($this->reply),
-                    'comment_id' => $comment_id,
-                    'user_id' => $user_id,
-                    'car_id' => 0,
-                    'news_id' => $newComment->id
-                ]);
-
-                $this->reset('reply');
-
-                $this->dispatch('renderReplyComments');
-            }
-        }
     }
 
-    public function getCommentsCar()
-    {
-        $carComment = Car::where('slug', $this->slug)->first();
-
-        return CommentsModel::with([
-            'user', 'reply' => function ($query) {
-                $query->orderBy('created_at', 'desc');
-            }
-        ])
-            ->where('car_id', $carComment->id)
-            ->orderBy('created_at', 'desc')
-            ->simplePaginate(4);
-    }
-
-    public function getCommentsNew()
-    {
-        $slug = str_replace('.html', '', $this->slug);
-        $newComment = News::where('slug', $slug)->first();
-
-        return CommentsModel::with([
-            'user', 'reply' => function ($query) {
-                $query->orderBy('created_at', 'desc');
-            }
-        ])
-            ->where('news_id', $newComment->id)
-            ->orderBy('created_at', 'desc')
-            ->simplePaginate(4);
-    }
-
-
-
-    #[On('renderComments')]
     public function render()
     {
-        if ($this->page == 'car-detail') {
-            $listComments = $this->getCommentsCar();
-        }
-
-        if ($this->page == 'news.index') {
-            $listComments = $this->getCommentsNew();
-        }
-
         return view('livewire.comments.list-comment', [
-            'listComments' => $listComments
+            'listComments' => $this->renderComment()
         ]);
     }
 }
